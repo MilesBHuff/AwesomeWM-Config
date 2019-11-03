524120675af84418f36f64c2fdc264a92ee50778
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2019-11-02 21:02:00 -0400

    Notifications rework

--- de/notifications.lua
+++ de/notifications.lua
@@ -1,37 +1,42 @@
-local naughty = require("naughty")
+-- Notification wrapper
+-- -----------------------------------------------------------------------------
+local notify = function(args)
+    args = args or {}
+    require("naughty").notify({
+		preset = args.preset and naughty.config.presets[args.preset] or nil,
+		title  = args.title and args.title or "Notification",
+		text   = args.text,
+    })
+end
 
--- Check if awesome encountered an error during startup and fell back to
--- another config (This code will only ever execute for the fallback config)
+-- Handle errors from another instance
+-- -----------------------------------------------------------------------------
+-- Check if awesome encountered an error during startup and fell back to another config
+-- (This code will only ever execute for the fallback config.)
 if awesome.startup_errors then
-    naughty.notify({
-		preset = naughty.config.presets.critical,
-		title = "Startup-error!",
-		text = awesome.startup_errors,
+    notify({
+		preset = "critical",
+		title  = "Startup Error!",
+		text   = awesome.startup_errors,
 	})
 end
 
 -- Handle runtime errors after startup
+-- -----------------------------------------------------------------------------
 do
     local in_error = false -- Make sure we don't go into an endless error loop
-    awesome.connect_signal("debug::error", function(err)
-
+    awesome.connect_signal("debug::error", function(error)
         if in_error then return end
         in_error = true
-
-        naughty.notify({
-            preset = naughty.config.presets.critical,
-            title = "Error!",
-            text = tostring(err)
+        notify({
+            preset = "critical",
+            title  = "Runtime Error!",
+            text   = tostring(error)
         })
         in_error = false
     end)
 end
 
--- Useful for debug text
-notify = function(text)
-    naughty.notify({
-		preset = naughty.config.presets.info,
-		title = "Notification",
-		text = text,
-    })
-end
+-- Export
+-- -----------------------------------------------------------------------------
+return notify
--- rc.lua
+++ rc.lua
@@ -1,20 +1,25 @@
 -- Variables
+-- -----------------------------------------------------------------------------
 local de = true
 local cwd = "/home/miles/.config/awesome/"
 
 -- Global imports
+-- -----------------------------------------------------------------------------
 pcall(require, "luarocks.loader") -- If LuaRocks is installed, make sure that packages installed through it are found. If LuaRocks is not installed, do nothing.
 awful = require("awful")
 
 -- Theme
+-- -----------------------------------------------------------------------------
 beautiful = require("beautiful")
 beautiful.init(cwd .. "theme/rc.lua")
 
 -- Prerequisites
-require("de/notifications")
+-- -----------------------------------------------------------------------------
+notify = require("de/notifications")
 require("wm/tags")
 
 -- Desktop environment
+-- -----------------------------------------------------------------------------
 if de then
     require("de/menu")
     wibox = require("wibox")
@@ -23,6 +28,7 @@ if de then
 end
 
 -- Window management
+-- -----------------------------------------------------------------------------
 require("awful.autofocus")
 require("wm/input")
 require("wm/rules")
