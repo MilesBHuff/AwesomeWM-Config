6c360c33f6d01485de852172fa0b84a4b8bc0133
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2020-02-16 21:42:37 -0500

    Fixed time display

--- de/widgets/datetime.lua
+++ de/widgets/datetime.lua
@@ -2,7 +2,16 @@
 --------------------------------------------------------------------------------
 local datetime = wibox.widget{
     widget  = wibox.widget.textclock,
-    format  = "<span font='Noto Sans 7.5'>%b %d</span>" .. "%n" .. "<span font='Noto Mono Bold 9.0'>%H:%M</span>" .. "<span font='Noto Sans 7.5'>UTC-4</span>", -- "%-d" and "UTC%-:::z"
+    format  =
+        "<span font='Noto Sans 7.5'>" ..
+            "%b %d" ..
+        "</span>%n" ..
+        "<span font='Noto Sans Mono Bold 9.0'>" ..
+            "%H:%M" ..
+        "</span>" ..
+        "<span font='Noto Sans 7.5'>" ..
+            "UTC%-:::z" ..
+        "</span>",
     align   = 'center',
     valign  = 'center',
 }
