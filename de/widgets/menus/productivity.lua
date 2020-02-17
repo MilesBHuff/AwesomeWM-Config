2fe17d8175a7262eb980506192d7a6d17e0daa99
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2020-02-16 21:00:48 -0500

    Revitalized the Productivity menu

--- de/widgets/menus/productivity.lua
+++ de/widgets/menus/productivity.lua
@@ -1,37 +1,51 @@
-local webbrowsers_menu = {
-	{"&Firefox",           env .. "firefox",                          nil},
-	{"&Chromium",          env .. "chromium",                         nil},
-	{"&Internet Explorer", env .. "playonlinux --run 'WineIexplore'", nil},
-}
-local office_menu = {
-	{"&Office suite",    env .. "libreoffice", beautiful.get_icon("libreoffice-main")},
-	{"&Email client",    env .. "thunderbird", nil},
-  --{"C&alendar",        env .. "",            beautiful.get_icon("calendar")},
-	{nil,                nil,                  nil},
-	{"&Mind-mapper",     env .. "vym",         nil},
-	{"&Document-viewer", env .. "okular",      nil},
-}
-local mapping_menu = {
-	{"&Virtual globe", env .. "google-earth",   beautiful.get_icon("globe-viewer")},
-	{"Map-&projector", env .. "flex-projector", nil},
-}
-local languages_menu = {
-  --{"&RosettaStone",       env .. "",      nil},
-	{"&Kurso de esperanto", env .. "kurso", nil},
+
+local communication_menu = {
+	{"&Webchat client",  env .. "franz",   beautiful.get_icon("franz")},
+	{"&IRC client",      env .. "hexchat", beautiful.get_icon("hexchat")},
+	{"I&M client",       env .. "pidgin",  beautiful.get_icon("applications-chat-panel")},
+	{nil,                nil,              nil},
+	{"&Discord",         env .. "discord", beautiful.get_icon("discord")},
+  --{"&Skype",           env .. "skype",   beautiful.get_icon("skype")},
 }
 local development_menu = {
-	{"T&ext-editor", env .. "code",     beautiful.get_icon("text-editor")},
-	{"&Java IDE",    env .. "eclipse",  nil},
-	{nil,            nil,               nil},
-	{"&Diff'-tool",  env .. "bcompare", beautiful.get_icon("diff-viewer")},
+	{"T&ext-editor",     env .. "code",           beautiful.get_icon("accessories-text-editor")},
+	{"&Hex-editor",      env .. "ghex",           beautiful.get_icon("ghex")},
+	{"&Diff viewer",     env .. "bcompare",       beautiful.get_icon("kompare")},
+	{nil,                nil,                     nil},
+	{"&Java IDE",        env .. "eclipse",        beautiful.get_icon("eclipse")},
+	{"&Android IDE",     env .. "android-studio", beautiful.get_icon("android-studio")},
+	{nil,                nil,                     nil},
+	{"&Enpoint-querier", env .. "postman",        beautiful.get_icon("postman")},
+}
+local education_menu = {
+    {"&RosettaStone",       env .. "",      beautiful.get_icon("rosettastone")},
+    {"&Kurso de esperanto", env .. "kurso", beautiful.get_icon("kurso")},
+}
+local mapping_menu = {
+	{"&Virtual globe", env .. "google-earth",   beautiful.get_icon("googleearth")},
+    {"Map &projector", env .. "flex-projector", beautiful.get_icon("")},
+}
+local office_menu = {
+	{"&Office suite",    env .. "libreoffice",    beautiful.get_icon("libreoffice-main")},
+	{"&Document viewer", env .. "okular",         beautiful.get_icon("accessories-document-viewer")},
+	{"&Notes",           env .. "standard-notes", beautiful.get_icon("gnome-sticky-notes-applet")},
+	{nil,                nil,                     nil},
+	{"&Email client",    env .. "thunderbird",    beautiful.get_icon("applications-mail")},
+    {"C&alendar",        env .. "",               beautiful.get_icon("calendar")},
+    {"&Contacts",        env .. "",               beautiful.get_icon("gnome-contacts")},
+}
+local webbrowsers_menu = {
+	{"&Secure browser",  env .. "firefox",                          beautiful.get_icon("firefox")},
+	{"&Vanilla browser", env .. "chromium",                         beautiful.get_icon("google-chrome")},
+    {"&Legacy browser",  env .. "playonlinux --run 'WineIexplore'", beautiful.get_icon("wine-browser")},
 }
 
 --------------------------------------------------------------------------------
 return {
-	{"&Web-browsers", webbrowsers_menu, beautiful.get_icon("web-browsers")},
-	{"&Office",       office_menu,      nil},
-	{nil,             nil,              nil},
-	{"&Development",  development_menu, nil},
-	{"&Languages",    languages_menu,   nil},
-	{"&Mapping",      mapping_menu,     nil},
+	{"&Web browsers",    webbrowsers_menu,     beautiful.get_icon("applications-webbrowsers")},
+	{"Communications",   communication_menu,   beautiful.get_icon("ekiga")},
+	{"&Office",          office_menu,          beautiful.get_icon("libreoffice-main")},
+	{"&Development",     development_menu,     beautiful.get_icon("applications-development")},
+    {"&Education",       education_menu,       beautiful.get_icon("applications-science")},
+	{"&Mapping",         mapping_menu,         beautiful.get_icon("applications-interfacedesign")},
 }
--- de/widgets/menus/utilities.lua
+++ de/widgets/menus/utilities.lua
@@ -1,22 +1,12 @@
-local communication_menu = {
-	{"&Webchat client",  env .. "franz",   beautiful.get_icon("franz")},
-	{"&IRC client",      env .. "hexchat", beautiful.get_icon("hexchat")},
-	{"I&M client",       env .. "pidgin",  beautiful.get_icon("applications-chat-panel")},
-	{nil,                nil,              nil},
-	{"&Discord",         env .. "discord", beautiful.get_icon("discord")},
-  --{"&Skype",           env .. "skype",   beautiful.get_icon("skype")},
-}
 return {
 	{"&Terminal",        env .. terminal,                                             beautiful.get_icon("utilities-terminal")},
 	{"&File-manager",    env .. "dolphin",                                            beautiful.get_icon("file-manager")},
 	{nil,                nil,                                                         nil},
+	{"&Authenticator",   env .. "chromium --app-id=gaedmjdfmmahhbjefcbgaolhhanlaolb", beautiful.get_icon("authy")},
 	{"C&haracter map",   env .. "gnome-character-map",                                beautiful.get_icon("accessories-character-map")},
 	{"&Calculator",      env .. "qalculate-gtk",                                      beautiful.get_icon("accessories-calculator")},
+	{nil,                nil,                                                         nil},
     {"A&rchiver",        env .. "xarchiver",                                          beautiful.get_icon("archive-manager")},
-	{nil,                nil,                                                         nil},
-	{"Communications",   communication_menu,                                          beautiful.get_icon("ekiga")},
-	{nil,                nil,                                                         nil},
-	{"&Authenticator",   env .. "chromium --app-id=gaedmjdfmmahhbjefcbgaolhhanlaolb", beautiful.get_icon("authy")},
 	{"&Torrent client",  env .. "deluge-gtk -q",                                      beautiful.get_icon("application-x-bittorrent")},
 	{"&Virtual machine", env .. "bash '" .. globals.cwd .. "lib/wrappers/vbox.bash'", beautiful.get_icon("virtualbox")},
 }
