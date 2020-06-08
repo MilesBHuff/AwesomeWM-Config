2dddb68e58e9fffd49eb1d23e5f3f91ea294ffb0
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2020-06-08 03:45:28 -0400

    Updated gcharmap

--- de/widgets/menus/utilities.lua
+++ de/widgets/menus/utilities.lua
@@ -3,7 +3,7 @@ return {
 	{"&File-manager",    cmd("dolphin"),                                            beautiful.get_icon("file-manager")},
 	{nil, nil, nil},
 	{"&Authenticator",   cmd("chromium --app-id=gaedmjdfmmahhbjefcbgaolhhanlaolb"), beautiful.get_icon("authy")},
-	{"C&haracter map",   cmd("gnome-character-map"),                                beautiful.get_icon("accessories-character-map")},
+	{"C&haracter map",   cmd("gucharmap"),                                          beautiful.get_icon("accessories-character-map")},
 	{"&Calculator",      cmd("qalculate-gtk"),                                      beautiful.get_icon("accessories-calculator")},
 	{nil, nil, nil},
     {"A&rchiver",        cmd("xarchiver"),                                          beautiful.get_icon("archive-manager")},
